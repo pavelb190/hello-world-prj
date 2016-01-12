d7db1042e30b509051e7316c79f660e443d54269
Katya <kettyqueen@yahoo.com>
2016-01-12 20:21:22 +0300

    add sum method

--- HelloWorldPrj/src/main/java/by/it/academy/App.java
+++ HelloWorldPrj/src/main/java/by/it/academy/App.java
@@ -9,6 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Ky-ky!" );
+		int a = 5;
+		int b = 6;
+		public int sum(a, b){
+			return a + b;
+		}
 
         // ...Some important comment...
     }
